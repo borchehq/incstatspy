f2e6a99d47f7fae9472afe3560d5aae04fa68f2f
borchehq <borchertvv@icloud.com>
2024-08-07 18:01:20 +0200

    Add rstatspy C extension for python.

---
+++ src/rstatspy/rstatspy.c
@@ -0,0 +1,28 @@
+#include <Python.h>
+
+#include "rstatspy.h"
+
+#define PY_SSIZE_T_CLEAN
+
+float square(float x) { return x * x; }
+
+static PyObject *square_wrapper(PyObject *self, PyObject *args) {
+  float input, result;
+  if (!PyArg_ParseTuple(args, "f", &input)) {
+    return NULL;
+  }
+  result = square(input);
+  return PyFloat_FromDouble(result);
+}
+
+static PyMethodDef example_methods[] = {
+    {"square", square_wrapper, METH_VARARGS, "Square function"},
+    {NULL, NULL, 0, NULL}};
+
+static struct PyModuleDef example_module = {PyModuleDef_HEAD_INIT, "example",
+                                             NULL, -1, example_methods};
+
+/* name here must match extension name, with PyInit_ prefix */
+PyMODINIT_FUNC PyInit_example(void) {
+  return PyModule_Create(&example_module);
+}
---
+++ src/rstatspy/rstatspy.h
@@ -0,0 +1,4 @@
+#ifndef RSTATS_PY_H
+#define RSTATS_PY_H
+
+#endif
