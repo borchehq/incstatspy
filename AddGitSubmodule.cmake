ef1fe90e5e73bdc4e22f7a676a1249ed69f05ebb
borchehq <borchertvv@icloud.com>
2024-08-08 08:49:51 +0200

    Search for CMakeLists.txt instead of .git.

--- AddGitSubmodule.cmake
+++ AddGitSubmodule.cmake
@@ -2,7 +2,7 @@ find_package(Git REQUIRED)
 
 function(add_git_submodule dir)
 
-    if(NOT EXISTS ${dir}/.git)
+    if(NOT EXISTS ${dir}/CMakeLists.txt)
     execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- ${dir}
         WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
         COMMAND_ERROR_IS_FATAL ANY)
